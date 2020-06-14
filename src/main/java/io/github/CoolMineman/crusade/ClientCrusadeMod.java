3f260248ed48f16fe6bd5f599eebf25261369199
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-06-13 21:24:40 -0500

    remove bad ideas they never happened

--- src/main/java/io/github/CoolMineman/crusade/ClientCrusadeMod.java
+++ src/main/java/io/github/CoolMineman/crusade/ClientCrusadeMod.java
@@ -1,6 +1,5 @@
 package io.github.CoolMineman.crusade;
 
-import io.github.CoolMineman.crusade.trebuchet.CursedModelGenerator;
 import io.github.CoolMineman.crusade.trebuchet.TrebuchetRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
@@ -10,8 +9,6 @@ import net.minecraft.client.render.RenderLayer;
 public class ClientCrusadeMod implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        //BlockRenderLayerMap.INSTANCE.putBlock(CrusadeMod.TREBUCHET_BASE, RenderLayer.getTranslucent());
         BlockEntityRendererRegistry.INSTANCE.register(CrusadeMod.TREBUCHET_ENTITY, TrebuchetRenderer::new);
-        CursedModelGenerator.doTheCursed();
     }
 }
