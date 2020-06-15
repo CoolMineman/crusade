a1d8b176c93398fbe8fc48cb4c6eb9e724da3569
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-06-15 15:08:00 -0500

    blessed

--- src/main/java/io/github/CoolMineman/crusade/mixin/ModelElementMixin.java
+++ src/main/java/io/github/CoolMineman/crusade/mixin/ModelElementMixin.java
@@ -3,7 +3,6 @@ package io.github.CoolMineman.crusade.mixin;
 import com.google.gson.JsonObject;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
