612961152c66bc8aae4258c6ada6faa4183cf167
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2020-06-19 14:31:50 -0500

    clean up import

--- src/main/java/io/github/CoolMineman/crusade/trebuchet/TrebuchetRenderer.java
+++ src/main/java/io/github/CoolMineman/crusade/trebuchet/TrebuchetRenderer.java
@@ -9,8 +9,6 @@ import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
-import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.text.LiteralText;
