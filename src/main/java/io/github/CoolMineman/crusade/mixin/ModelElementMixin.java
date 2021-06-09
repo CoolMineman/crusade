cbf09ac85432a71685e58fa5922e6399deddf7bd
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2021-06-09 12:38:46 -0500

    mixin is stupid

--- src/main/java/io/github/CoolMineman/crusade/mixin/ModelElementMixin.java
+++ src/main/java/io/github/CoolMineman/crusade/mixin/ModelElementMixin.java
@@ -15,12 +15,12 @@ public abstract class ModelElementMixin {
     @Shadow
     abstract Vec3f deserializeVec3f(JsonObject object, String name);
 
-    @Inject(method = "net/minecraft/client/render/model/json/ModelElement$Deserializer.deserializeTo(Lcom/google/gson/JsonObject;)Lnet/minecraft/client/util/math/Vector3f;", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "net/minecraft/client/render/model/json/ModelElement$Deserializer.deserializeTo(Lcom/google/gson/JsonObject;)Lnet/minecraft/util/math/Vec3f;", at = @At("HEAD"), cancellable = true)
     private void deserializeTo(JsonObject object, CallbackInfoReturnable<Vec3f> cb) {
         cb.setReturnValue(deserializeVec3f(object, "to"));
     }
 
-    @Inject(method = "net/minecraft/client/render/model/json/ModelElement$Deserializer.deserializeFrom(Lcom/google/gson/JsonObject;)Lnet/minecraft/client/util/math/Vector3f;", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "net/minecraft/client/render/model/json/ModelElement$Deserializer.deserializeFrom(Lcom/google/gson/JsonObject;)Lnet/minecraft/util/math/Vec3f;", at = @At("HEAD"), cancellable = true)
     private void deserializeFrom(JsonObject object, CallbackInfoReturnable<Vec3f> cb) {
         cb.setReturnValue(deserializeVec3f(object, "from"));
     }
